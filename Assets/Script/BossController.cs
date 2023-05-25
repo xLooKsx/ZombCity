6f0ad8859bdbbe476cd6248f2b5290b063d46b98
xLooKsx <lu.oliveira.silva98@gmail.com>
2023-05-25 19:59:51 +0100

    New assets added to project Boss created with navMesh and navMeshAgent

---
+++ Assets/Script/BossController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController : MonoBehaviour {
+
+    private Transform player;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        this.navMeshAgent.SetDestination(this.player.position);
+    }
+}
